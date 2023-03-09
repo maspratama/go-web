7cc172e8e0290ec8393eced4b3889ba9d157f3f7
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-03-10 06:17:59 +0700

    develope handler_test

---
+++ handler_test.go
@@ -0,0 +1,27 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		//logic web
+		_, err := fmt.Fprint(w, "Hello World")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
